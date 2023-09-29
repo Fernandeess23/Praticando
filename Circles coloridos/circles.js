a00726c20028d3d2caa4b6ae4f834e25d8793cfe
Lucas Lima Fernandes <LKpiramideLK@gmail.com>
2023-09-28 23:22:53 -0300

    Update circles.js

--- Circles coloridos/circles.js
+++ Circles coloridos/circles.js
@@ -5,12 +5,11 @@ render(15, 180, '#AE445A') +
 render(15, 90, '#2E8A99') +
 render(15, 270, '#F39F5A')
 
-function render(itens, offset = 0, color = "red") {
-    const rad = 180 / itens;
-    const delay = 4000 / itens
+function render(items, offset = 0, color = "red") {
+    const rad = 180 / items;
+    const delay = 4000 / items
 
-    return [...Array(itens).keys()]
-
+    return [...Array(items).keys()]
     .map(
         (index) => `<i style="--_bg:${color};--_deg:
         ${rad * index + offset}deg;--_del:${delay * index}
